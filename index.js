a8a31aed042668e34f4dfd4d912cdeeca337f4f7
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-29 16:29:14 -0500

    analytics

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, Analytics} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
+Amplify.Logger.LOG_LEVEL = 'DEBUG';
+Analytics.updateEndpoint({
+  attributes: {
+    hobbies: ['hiking', 'reading'],
+  },
+});
 
 AppRegistry.registerComponent(appName, () => App);
