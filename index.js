795c8553b67fe712e2ee3ca2846e922ad1a4f9e2
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-29 16:44:43 -0500

    auth

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, Auth} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
+Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+async function signIn() {
+  try {
+    const user = await Auth.signIn('testuser20', 'testtest');
+    console.log({user});
+  } catch (error) {
+    console.log('error signing in', error);
+  }
+}
+signIn();
 
 AppRegistry.registerComponent(appName, () => App);
