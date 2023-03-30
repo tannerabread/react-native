ce38a081c4856c46376af5bad59ea536a9fe5c7e
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 15:20:10 -0500

    ccache

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, Cache} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+Cache.setItem('testKey', 'testValue');
+Cache.getItem('testKey').then(value => {
+  console.log({value});
+});
 
 AppRegistry.registerComponent(appName, () => App);
