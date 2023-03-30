127a0e62d49a082329dd93c604bada3418ffb3a6
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 13:16:59 -0500

    push notification

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ import App from './App';
 import {name as appName} from './app.json';
 
 import {Amplify} from 'aws-amplify';
+import {PushNotification} from '@aws-amplify/pushnotification';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+PushNotification.onRegister(token => {
+  console.log('in app registration', token);
+});
+
+PushNotification.onNotificationOpened(notification => {
+  console.log('the notification is opened', notification);
+});
 
 AppRegistry.registerComponent(appName, () => App);
