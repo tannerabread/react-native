f4b9c448c090d998572fe4b907a82e811595bfd5
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 15:34:15 -0500

    hub

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, Hub} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+Hub.listen('DogsChannel', data => {
+  const {payload} = data;
+  console.log({payload});
+});
+
+Hub.dispatch('DogsChannel', {
+  event: 'buttonClick',
+  data: {color: 'blue'},
+  message: 'testMessage',
+});
 
 AppRegistry.registerComponent(appName, () => App);
