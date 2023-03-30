b166c5d76878017b5877366235340123b098e179
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 12:53:06 -0500

    pubsub incomplete

--- index.js
+++ index.js
@@ -6,9 +6,42 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, PubSub, Auth} from 'aws-amplify';
+import {AWSIoTProvider} from '@aws-amplify/pubsub';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+Amplify.addPluggable(
+  new AWSIoTProvider({
+    aws_pubsub_region: 'us-east-1',
+    aws_pubsub_endpoint:
+      'wss://a2b5xt8ljhusrb-ats.iot.us-east-1.amazonaws.com/mqtt',
+  }),
+);
+
+async function signIn() {
+  try {
+    const user = await Auth.signIn('testuser20', 'testtest');
+    console.log({user});
+    Auth.currentCredentials().then(info => {
+      const cognitoIdentityId = info.identityId;
+      console.log({cognitoIdentityId});
+    });
+  } catch (error) {
+    console.log('error signing in', error);
+  }
+}
+signIn();
+
+PubSub.subscribe('myTopic').subscribe({
+  next: data => console.log('Message received', data),
+  error: error => console.error(error),
+  complete: () => console.log('Done'),
+});
+async function publish() {
+  await PubSub.publish('myTopic', {msg: 'Hello to all subscribers!'});
+}
+publish();
 
 AppRegistry.registerComponent(appName, () => App);
