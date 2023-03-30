4743051f313d5c737734248c4c120d7cb6f2dbce
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 15:45:14 -0500

    i18n

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, I18n} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+I18n.setLanguage('fr');
+const dict = {
+  fr: {
+    'Sign In': 'Se connecter',
+    'Sign Up': "S'inscrire",
+  },
+  es: {
+    'Sign In': 'Registrarse',
+    'Sign Up': 'Regístrate',
+  },
+};
+I18n.putVocabularies(dict);
+console.log(I18n.get('Sign In'));
 
 AppRegistry.registerComponent(appName, () => App);
