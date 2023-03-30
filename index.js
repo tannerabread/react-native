c089fd7b38630ce3776b7da7cef2e55d857cc0a0
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 14:33:38 -0500

    storage

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, Storage} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+async function putFile() {
+  const result = await Storage.put('test.txt', 'Hello');
+  console.log(result);
+}
+putFile();
+
+Storage.list('') // for listing ALL files without prefix, pass '' instead
+  .then(({results}) => console.log(results))
+  .catch(err => console.log(err));
 
 AppRegistry.registerComponent(appName, () => App);
