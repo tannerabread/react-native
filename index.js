8ff5c024c97fe79987604a7ab35b8c79015ada97
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 08:13:30 -0500

    logger

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ import {name as appName} from './app.json';
 import {Amplify} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
+Amplify.Logger.LOG_LEVEL = 'DEBUG';
 
 AppRegistry.registerComponent(appName, () => App);
