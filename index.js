1b2384b2acde16f83ce50feeaf63b601b48a2ffc
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 08:13:08 -0500

    in-app-messaging

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 /**
  * @format
  */
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, Notifications} from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
+Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+const {InAppMessaging} = Notifications;
+InAppMessaging.syncMessages();
 
 AppRegistry.registerComponent(appName, () => App);
