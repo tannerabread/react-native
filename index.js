6fd820191fa4f95e653367a4f148c9d287fd48f2
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 16:28:57 -0500

    graphql todo

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {Amplify} from 'aws-amplify';
+import {Amplify, API, graphqlOperation} from 'aws-amplify';
+import {createTodo} from './graphql/mutations';
+import {listTodos} from './graphql/queries';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
+async function getListTodos() {
+  const todo = {name: 'My first todo', description: 'Hello world!'};
+  await API.graphql(graphqlOperation(createTodo, {input: todo}));
+  const todos = await API.graphql(graphqlOperation(listTodos));
+  console.log(todos.data.listTodos.items);
+}
+getListTodos();
 
 AppRegistry.registerComponent(appName, () => App);
