924e61d8464c872bba4cf3d2788b320e6fc26203
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-03-30 11:22:31 -0500

    predictions

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ import {name as appName} from './app.json';
 
 import {Amplify} from 'aws-amplify';
 import config from './aws-exports';
+import {
+  Predictions,
+  AmazonAIPredictionsProvider,
+} from '@aws-amplify/predictions';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
+Predictions.addPluggable(new AmazonAIPredictionsProvider());
+
+Predictions.convert({
+  translateText: {
+    source: {
+      text: 'Hello World',
+    },
+  },
+})
+  .then(result => console.log({result}))
+  .catch(err => console.log({err}));
 
 AppRegistry.registerComponent(appName, () => App);
