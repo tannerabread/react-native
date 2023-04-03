f8832a4ec2a7bd7f1251c884ed8534d3897e7917
Bannon Tanner <bannon.n.tanner@gmail.com>
2023-04-03 18:02:04 -0500

    added ios specific call

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ import {name as appName} from './app.json';
 
 import {Amplify} from 'aws-amplify';
 import {PushNotification} from '@aws-amplify/pushnotification';
+import {PushNotificationIOS} from 'react-native-community/push-notification-ios';
 import config from './aws-exports';
 Amplify.configure(config);
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
 
 PushNotification.onRegister(token => {
   console.log('in app registration', token);
+});
+
+PushNotification.onNotification(notification => {
+  console.log('in app notification', notification);
+
+  // required on iOS only
+  notification.finish(PushNotificationIOS.FetchResult.NoData);
 });
 
 PushNotification.onNotificationOpened(notification => {
